cc8445aff5674ecd7025d33cdc31566fa408098b
anvian76 <anvian76@gmail.com>
2025-02-22 13:45:24 +0500

    created customer model and made migration

--- backend/invoices/models.py
+++ backend/invoices/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    title = models.CharField(max_length=120)
+    tax_id = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+
+    def _str_(self):
+        return self.title
