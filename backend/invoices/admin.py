1f14062e6153cf94740a722478425d0d270b84e3
anvian76 <anvian76@gmail.com>
2025-02-22 13:48:48 +0500

    registered Customer model to admin

--- backend/invoices/admin.py
+++ backend/invoices/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Customer
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'tax_id', 'address')
 
 # Register your models here.
+
+admin.site.register(Customer, CustomerAdmin)
