8b554c93190b7f5dbf9c27b0a7b024e912a3f81f
anvian76 <anvian76@gmail.com>
2025-02-22 14:13:45 +0500

    created view specified urls, step 2 api building finished

--- backend/invoices/serializers.py
+++ backend/invoices/serializers.py
@@ -4,4 +4,4 @@ from .models import Customer
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('title', 'tax_id', 'address')
+        fields = ('id','title', 'tax_id', 'address')
--- backend/invoices/views.py
+++ backend/invoices/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CustomerSerializer
+from .models import Customer
 
 # Create your views here.
+
+class CustomerView(viewsets.ModelViewSet):
+    serializer_class = CustomerSerializer
+    queryset = Customer.objects.all()
